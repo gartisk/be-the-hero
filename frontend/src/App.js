7b70247f12639f9f481a9af388fca9a236bf4f78
Guilherme Araujo <gartisk@gmail.com>
2020-04-10 16:45:23 -0300

    Add frontend da aplicação

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './global.css';
+
+// import Logon from './pages/Logon';
+import Router from './routes';
+
+function App() {
+   return (
+    <Router />
+  );
+}
+
+export default App;
